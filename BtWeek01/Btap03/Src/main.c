1a4fc9f6157a9691f0ed21ddc67d5ff20522af33
kientaminh <taminhkien311@gmail.com>
2026-09-09 18:34:05 +0700

    Sửa bài 3

--- BtWeek01/Btap03/Src/main.c
+++ BtWeek01/Btap03/Src/main.c
@@ -1,181 +1,115 @@
+#include <stdint.h>
 // RCC
 #define RCC_CR          (*(volatile unsigned int *)0x40021000)
 #define RCC_CFGR        (*(volatile unsigned int *)0x40021004)
 #define RCC_APB2ENR     (*(volatile unsigned int *)0x40021018)
 
-// FLASH
 #define FLASH_ACR       (*(volatile unsigned int *)0x40022000)
 
-// GPIOA
 #define GPIOA_CRL       (*(volatile unsigned int *)0x40010800)
-#define GPIOA_CRH       (*(volatile unsigned int *)0x40010804)
-#define GPIOA_IDR       (*(volatile unsigned int *)0x40010808)
 #define GPIOA_BSRR      (*(volatile unsigned int *)0x40010810)
+#define GPIOA_ODR       (*(volatile unsigned int *)0x4001080C)
 
-// SysTick
+#define GPIOB_CRH       (*(volatile unsigned int *)0x40010C04)
+#define GPIOB_IDR       (*(volatile unsigned int *)0x40010C08)
+#define GPIOB_BSRR      (*(volatile unsigned int *)0x40010C10)
+
 #define SYST_CSR        (*(volatile unsigned int *)0xE000E010)
 #define SYST_RVR        (*(volatile unsigned int *)0xE000E014)
 #define SYST_CVR        (*(volatile unsigned int *)0xE000E018)
 
 
-/* 
- * CLOCK: 8 MHz HSE - 72 MHz SYSCLK
- */
 static void clock_init(void)
 {
-    /*
-     * 72 MHz cần 2 wait states cho FLASH
-     *
-     * LATENCY[2:0] = 010
-     */
+    // FLASH: 2 wait states
     FLASH_ACR &= ~0x7U;
     FLASH_ACR |=  0x2U;
 
-
-    /*
-     * Bật HSE
-     *
-     * RCC_CR bit 16 = HSEON
-     */
+    // Enable HSE
     RCC_CR |= (1U << 16);
 
-    /*
-     * Chờ HSE ổn định
-     *
-     * RCC_CR bit 17 = HSERDY
-     */
+    // Wait HSE ready
     while (!(RCC_CR & (1U << 17)))
     {
     }
 
-
     /*
-     * Cấu hình:
-     *
-     * AHB  = SYSCLK / 1 = 72 MHz
-     * APB1 = HCLK / 2   = 36 MHz
-     * APB2 = HCLK / 1   = 72 MHz
-     *
+     * APB1 = HCLK / 2 = 36 MHz
      * PLL source = HSE
-     * PLL multiplier = x9
+     * PLL = x9
      */
-
     RCC_CFGR = 0;
 
-    /* APB1 prescaler = /2
-       PPRE1 = 100
-    */
+    // APB1 prescaler /2
     RCC_CFGR |= (4U << 8);
 
-    /* PLL source = HSE */
+    // PLL source = HSE
     RCC_CFGR |= (1U << 16);
 
-    /* PLL multiplier = x9
-       PLLMUL = 0111
-    */
+    // PLL multiplier x9
     RCC_CFGR |= (7U << 18);
 
-
-    /*
-     * Bật PLL
-     */
+    // Enable PLL
     RCC_CR |= (1U << 24);
 
-    /*
-     * Chờ PLL lock
-     */
+    // Wait PLL ready
     while (!(RCC_CR & (1U << 25)))
     {
     }
 
-
-    /*
-     * Chọn PLL làm SYSCLK
-     *
-     * SW = 10
-     */
+    // Select PLL as SYSCLK
     RCC_CFGR &= ~(3U << 0);
     RCC_CFGR |=  (2U << 0);
 
-
-    /*
-     * Chờ hệ thống chuyển sang PLL
-     *
-     * SWS = 10
-     */
+    // Wait until PLL is SYSCLK
     while ((RCC_CFGR & (3U << 2)) != (2U << 2))
     {
     }
 }
 
 
-/* 
- * SysTick
- */
 static void systick_init(void)
 {
-    /*
-     * CPU = 72 MHz
-     *
-     * 1 ms:
-     *
-     * 72,000,000 / 1000 = 72,000
-     *
-     * Reload = 72000 - 1
-     */
+    // 72 MHz / 1000 = 72000 cycles = 1 ms
     SYST_RVR = 72000U - 1U;
 
-    /* Clear current value */
     SYST_CVR = 0;
 
-    /*
-     * bit 0 ENABLE    = 1
-     * bit 1 TICKINT   = 0   (không dùng interrupt)
-     * bit 2 CLKSOURCE = 1   (CPU clock = 72 MHz)
-     */
+    // ENABLE = 1
+    // TICKINT = 0
+    // CLKSOURCE = CPU clock
     SYST_CSR = (1U << 2) | (1U << 0);
 }
 
 
-/* 
- * Delay 
- */
 static void delay_ms(unsigned int ms)
 {
     while (ms--)
     {
-        /*
-         * COUNTFLAG = bit 16
-         * được set khi SysTick đếm từ 0 về reload
-         */
         while (!(SYST_CSR & (1U << 16)))
         {
         }
     }
 }
 
+
 static void gpio_init(void)
 {
-    // Enable GPIOA clock
+    /*
+     * Enable:
+     * GPIOA clock = bit 2
+     * GPIOB clock = bit 3
+     */
     RCC_APB2ENR |= (1U << 2);
+    RCC_APB2ENR |= (1U << 3);
 
-    GPIOA_CRL = 0x44444444;
-    GPIOA_CRH = 0x22222222;
-}
 
-/*
- * Đọc PA0 - PA7
- */
-static unsigned char read_input(void)
-{
-    return (unsigned char)(GPIOA_IDR & 0xFF);
-}
+    GPIOA_CRL = 0x22222222;
 
-static void write_output(unsigned char data)
-{
+
     
-    GPIOA_BSRR = (0xFFU << 16);
-    GPIOA_BSRR = ((unsigned int)data << 8);
+    GPIOB_CRH = 0x88888888;
+    GPIOB_BSRR = 0x0000FF00;
 }
 
 
@@ -187,8 +121,13 @@ int main(void)
 
     while (1)
     {
-        unsigned char data = read_input();
-        data = ~data;
-        write_output(data);
+        uint32_t input;
+        uint32_t output;
+
+        input = (GPIOB_IDR >> 8) & 0xFF;
+
+        output = input;
+
+        GPIOA_ODR = (GPIOA_ODR & 0xFFFFFF00) | output;
     }
 }
